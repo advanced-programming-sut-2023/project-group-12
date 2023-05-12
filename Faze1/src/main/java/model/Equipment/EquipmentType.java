9cc5e96127583e8eed375d430c620cc1ff3f29cb
MahoneDF <h1337408davo@gmail.com>
2023-05-12 16:59:29 +0330

    equipmentClass complete!

--- Faze1/src/main/java/model/Equipment/EquipmentType.java
+++ Faze1/src/main/java/model/Equipment/EquipmentType.java
@@ -2,12 +2,13 @@ package model.Equipment;
 
 public enum EquipmentType {
     //name, hitPoint, engineersCount, damage, range, speed, cost
-    PORTABLE_SHIELD("portable shield", 200,1,0,0,0,20),
+    FIRE_BALLISTA("fire ballista",500,2,400,5,2,100),
     BATTERING_RAM("battering ram",300,4,500,1,1,50),
     SIEGE_TOWER("siege tower", 400,4,0,0,0,150),
-    CATAPULT("catapult", 600,2,250,12,1,75), //manjenigh moteharek
     TREBUCHET("trebuchet",800,3,400,15,0,100), //manjenigh sabet
-    FIRE_BALLISTA("fire ballista",500,2,400,5,2,100)
+    PORTABLE_SHIELD("portable shield", 200,1,0,0,0,20),
+    CATAPULT("catapult", 600,2,250,12,1,75), //manjenigh moteharek
+
 
 
     ;
