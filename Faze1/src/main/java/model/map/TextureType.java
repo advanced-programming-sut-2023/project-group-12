cba0569d6c1226ba16fc38c084dd890f63943d6b
Mahdi-Shah <mahdi83shah@gmail.com>
2023-06-29 11:38:23 +0330

    add texture images

--- Faze1/src/main/java/model/map/TextureType.java
+++ Faze1/src/main/java/model/map/TextureType.java
@@ -1,33 +1,39 @@
 package model.map;
 
+import java.awt.*;
+import javafx.scene.image.Image;
+
+
 public enum TextureType {
-    EARTH(true, false),
-    GRAVEL(true, false),
-    STONE_MINE(true, false),
-    ROCK_NORTH(false, false),
-    ROCK_EAST(false, false),
-    ROCK_WEST(false, false),
-    ROCK_SOUTH(false, false),
-    IRON_MINE(true, false),
-    GRASS(true, false),
-    MEADOW(true, false),
-    DENSE_MEADOW(true, false),
-    OIL(true, true),
-    PLAIN(true, true),
-    SHALLOW_WATER(true, true),
-    FOUNTAIN(false, true),
-    SMALL_POND(false, true),
-    BIG_POND(false, true),
-    BEACH(true, true),
-    SEA(false, true);
+    EARTH(true, false, new Image(Cell.class.getResource("/images/ground.jpg").toExternalForm())),
+    GRAVEL(true, false, new Image(Cell.class.getResource("/images/Mountain1.png").toExternalForm())),
+    STONE_MINE(true, false, new Image(Cell.class.getResource("/images/Mountain1.png").toExternalForm())),
+    ROCK_NORTH(false, false, new Image(Cell.class.getResource("/images/stone.jpg").toExternalForm())),
+    ROCK_EAST(false, false, new Image(Cell.class.getResource("/images/stone.jpg").toExternalForm())),
+    ROCK_WEST(false, false, new Image(Cell.class.getResource("/images/stone.jpg").toExternalForm())),
+    ROCK_SOUTH(false, false, new Image(Cell.class.getResource("/images/stone.jpg").toExternalForm())),
+    IRON_MINE(true, false, new Image(Cell.class.getResource("/images/ironGround.jpg").toExternalForm())),
+    GRASS(true, false, new Image(Cell.class.getResource("/images/grass.jpg").toExternalForm())),
+    MEADOW(true, false, new Image(Cell.class.getResource("/images/grassland2.jpg").toExternalForm())),
+    DENSE_MEADOW(true, false, new Image(Cell.class.getResource("/images/grass.jpg").toExternalForm())),
+    OIL(true, true, new Image(Cell.class.getResource("/images/ground.jpg").toExternalForm())),
+    PLAIN(true, true, new Image(Cell.class.getResource("/images/plain.jpg").toExternalForm())),
+    SHALLOW_WATER(true, true, new Image(Cell.class.getResource("/images/landstone.png").toExternalForm())),
+    FOUNTAIN(false, true, new Image(Cell.class.getResource("/images/sea.jpg").toExternalForm())),
+    SMALL_POND(false, true, new Image(Cell.class.getResource("/images/gulf_tile.jpg").toExternalForm())),
+    BIG_POND(false, true, new Image(Cell.class.getResource("/images/gulf_tile.jpg").toExternalForm())),
+    BEACH(true, true, new Image(Cell.class.getResource("/images/Plain2.jpg").toExternalForm())),
+    SEA(false, true, new Image(Cell.class.getResource("/images/sea_tile.jpg").toExternalForm()));
 
     private final boolean passable;
 
     private final boolean isWateryLand;
+    private Image image;
 
-    TextureType(boolean passable, boolean isWateryLand) {
+    TextureType(boolean passable, boolean isWateryLand, Image image) {
         this.passable = passable;
         this.isWateryLand = isWateryLand;
+        this.image = image;
     }
 
     public boolean isWateryLand() {
